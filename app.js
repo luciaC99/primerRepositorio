a8365cf3bde5e63f14972c5342de956d5a751a1c
luciaC99 <luciaivanovich@gmail.com>
2023-05-11 19:15:49 -0300

    mi primer commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log (8); 
+let autosImportados =
+require('./autos');
+  const concesionaria = {
+     autos: autosImportados, buscarAuto:
+     function(pat) {
+        for (let 
+        i=0;i<this.autos.length;i++);
+        {
+           if (this.autos[i].patente==pat);
+           {
+              return this.autos[i];
+           }
+         }
+         return null;
+      }
+  };
